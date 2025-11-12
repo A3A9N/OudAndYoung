0f82e11586607e9f2faee542cebd2a38c0e63e1e
huseyinsr <39071@ma-web.nl>
2025-11-12 12:18:43 +0100

    trappen
    Child kan nu klimmen.

---
+++ YoungAndOld/Assets/Scripts/Trap/Trap.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField] private float climbForce = 6f;
+    private Rigidbody2D rb;
+    private bool isInLadderZone = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (CompareTag("Child") && isInLadderZone)
+        {
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, climbForce);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Trap"))
+        {
+            isInLadderZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Trap"))
+        {
+            isInLadderZone = false;
+        }
+    }
+}
